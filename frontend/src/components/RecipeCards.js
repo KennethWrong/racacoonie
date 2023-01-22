ea5acd451a102ec7202a86643ae1fcb21b45aaee
werunm <cankennethwong@gmail.com>
2023-01-22 01:19:45 -0500

    feat: aded saved button

--- frontend/src/components/RecipeCards.js
+++ frontend/src/components/RecipeCards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Recipe.css';
 import { useNavigate } from 'react-router-dom';
+import SaveButton from "../components/SaveButton";
 
 const RecipeCards = (props) => {
   const navigate = useNavigate();
@@ -8,12 +9,17 @@ const RecipeCards = (props) => {
     <div className='recipe-board'>
       {props.recipes
         ? props.recipes.map((recipe) => (
-          <div key={recipe.id} className='recipe-card g-g-col-4' onClick={() => { navigate('/recipe/' + recipe.id); }}>
-            <div className='recipe-image'>
+          <div key={recipe.id} className='recipe-card g-g-col-4'>
+            <div className='recipe-image hover:cursor-pointer' onClick={() => { navigate('/recipe/' + recipe.id); }}>
               image
             </div>
             <div className='recipe-details'>
-              <p className='recipe-name'>{recipe.name}</p>
+              <p className='recipe-name'>
+                {recipe.name}
+              </p>
+              <span>
+                <SaveButton rid={recipe['id']}/>
+              </span>
               <hr className='recipe-hr' />
               <p className='recipe-description'>{recipe.description}</p>
             </div>
---
+++ frontend/src/components/SaveButton.js
@@ -0,0 +1,84 @@
+import IconButton from '@mui/material/IconButton';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import { Box } from '@mui/system';
+
+
+export default function SaveButton ({rid, setRefresh, refresh}) {
+
+    const [liked, setLiked] = useState()
+
+    useEffect(() => {
+        getSavedRecipe()
+    }, [])
+
+    const getSavedRecipe = async () => {
+        try{
+            const config = {
+                headers:{
+                  authorization: localStorage.getItem("racacoonie-auth-token")
+                }
+              };
+            axios.get(`http://localhost:8000/user/liked`, config).then(
+                (res) => {
+                    const savedRecipes = res['data']
+                    if(savedRecipes){
+                        savedRecipes.forEach((r) => {
+                            if(r['id'] == rid) {
+                                setLiked(true);
+                                return;
+                            }
+                        })
+                    }
+                }
+            )
+        } catch (err){
+            console.log(err)
+            return err
+        }
+    }
+
+
+    const handleLikefunction = (e) => {
+        e.preventDefault();
+        const config = {
+            headers:{
+              authorization: localStorage.getItem("racacoonie-auth-token")
+            }}
+        axios.post('http://localhost:8000/recipe/like', {'rid':rid}, config).then((res) => {
+            setLiked(true)
+        })
+    }
+
+    const handleUnlikefunction = (e) => {
+        e.preventDefault();
+
+        if(refresh !== null) {
+            setRefresh(!refresh);
+        }
+
+        const config = {
+            headers:{
+              authorization: localStorage.getItem("racacoonie-auth-token")
+            }}
+            
+            axios.post('http://localhost:8000/recipe/unlike', {'rid':rid}, config).then((res) => {
+            setLiked(false)
+        })
+    }
+
+    return (
+        <div>
+        {liked?
+        <IconButton onClick={handleUnlikefunction}>
+            <FavoriteIcon color="error"/>
+        </IconButton> :
+        <IconButton onClick={handleLikefunction}>
+            <FavoriteBorderIcon color="error"/>
+        </IconButton>
+        }
+        </div>
+    )
+}
